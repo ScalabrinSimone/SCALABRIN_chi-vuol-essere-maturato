191e6adf0e1c0f41b385bc2d227b3d85faf0fae9
Simo <10933919@itisrossi.vi.it>
2025-09-24 10:28:49 +0200

    Downloaded Gson library with maven.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Usiamo librerie HttpClient che da java21 è dentro e Gson (da installare sulla cartella del progetto per renderla
+// disponibile anche a chi la pulla).
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
